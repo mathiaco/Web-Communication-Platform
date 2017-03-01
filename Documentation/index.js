4142bd5e2536cedddf6ccede9140f7ca4aa1d1c2
mathiaco <mathiaco2@gmail.com>
2017-03-01 03:27:50 -0500

    Update index.js

--- Documentation/index.js
+++ Documentation/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require ('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
+
+
+router.post('/username',function(req,res){
+    res.render('username', { username: req.body.username});
+    fs.writeFileSync('user.json','"'+req.body.username+'"')
+});
+
 
 module.exports = router;
