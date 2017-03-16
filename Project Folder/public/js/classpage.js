b413b113416b3774ac62884eab1b5837801bcb49
Jeff Buonamici <jbuonamici@outlook.com>
2017-03-15 22:51:06 -0400

    You are now able to add and delete users from classes
    #35 #34

--- Project Folder/public/js/classpage.js
+++ Project Folder/public/js/classpage.js
@@ -11,22 +11,174 @@ function initializeFirebase() {
 }
 
 function initializePage() {
+  refClassUsers = firebase.database().ref("classes/" + classID + "/users/");
+
   firebase.database().ref("classes/" + classID).once("value").then(function (snapshot) {
     $("#classTitle").text(snapshot.val().title);
-    var refClassUsers = firebase.database().ref("classes/" + classID + "/users/").orderByValue();
-    refClassUsers.once("value").then(function (childSnapshot) {
+    /*refClassUsers.once("value").then(function (childSnapshot) {
       childSnapshot.forEach(function (childChildSnapshot) {
         memberCount++;
         $("#classMembers").append(
           "<a href='#' class='list-group-item'>" +
-          childChildSnapshot.val() +
+          childChildSnapshot.val().username +
           "</a>"
         )
-      })
+      });
       $("#memberCount").text(memberCount);
-    })
-  })
-  
+    });*/
+  });
+
+  if (isTA()) {
+    $("#addUserBtnHolder").append(
+      "<a href='#' id='addUserBtn' class='btn btn-success btn pull-right' data-toggle='modal' data-target='#addUsersModal'>Add User</a>" +
+      "<div class='modal fade' id='addUsersModal' tabindex='-1' role='dialog' aria-labelledby='addUsersModalLabel'>" +
+      "<div class='modal-dialog' role='document'>" +
+      "<div class='modal-content'>" +
+      "<div class='modal-header'>" +
+      "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+      "<h4 class='modal-title' id='addUsersModalLabel'>Add user</h4>" +
+      "</div>" +
+      "<div class='modal-body'>" +
+
+      "<div class='row'>" +
+      "<div class='col-lg-12'>" +
+      "<div class='input-group custom-search-form' style='padding-bottom:20px;'>" +
+      "<input id='searchUserInput' type='text' class='form-control' placeholder='Search...'>" +
+      "<span class='input-group-btn'>" +
+      "<button id='searchUserBtn' class='btn btn-success' type='button'>" +
+      "<i class='fa fa-plus'></i>" +
+      "</button>" +
+      "</span>" +
+      "</div>" +
+      "<!-- /input-group -->" +
+      "</div>" +
+      "<!-- /.col-lg-6 -->" +
+      "</div>" +
+
+      "<div class='panel panel-default'>" +
+      "<div class='panel-heading'>" +
+      "<i class='fa fa-user fa-fw'></i> Current Users" +
+      "</div>" +
+      "<!-- /.panel-heading -->" +
+      "<div class='panel-body'>" +
+      "<div class='list-group' id='userList'>" +
+      "</div >" +
+      "<!-- /.list-group -->" +
+      "</div >" +
+      "<!-- /.panel-body -->" +
+      "</div >" +
+      "<!-- /.panel -->" +
+      "</div>" +
+      "<div class='modal-footer'>" +
+      "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>" +
+      "</div>" +
+      "</div>" +
+      "</div>" +
+      "</div>" +
+      "<!-- /.modal -->"
+    );
+    /*$("#addUserBtn").click(function () {
+      $("#userList").empty();
+      refClassUsers.once("value").then(function (childSnapshot) {
+        childSnapshot.forEach(function (childChildSnapshot) {
+          $("#userList").append(
+            "<a href='#' class='deleteUser list-group-item'>" +
+            "<span class='userName'>" + childChildSnapshot.val().username + "</span>" +
+            "<button class='pull-right btn btn-danger btn-xs'>Delete</button>" +
+            "</a>"
+          )
+        })
+        $("#memberCount").text(memberCount);
+        $(".deleteUser").click(function () {
+          alert($(this).find(".userName").text());
+          var userName = $(this).find(".userName").text();
+          refClassUsers.orderByChild("username").equalTo(userName).once("value").then(function (user1) {
+            user1.forEach(function (user2) {
+              refClassUsers.child(user2.key).remove();
+            })
+          })
+        })
+      })
+    })*/
+
+
+  }
+
+  refClassUsers.orderByChild("username").on("child_added", function (snapshot, prevChildKey) {
+    memberCount++;
+    var user = snapshot.val()
+    if (isFirstLoad) {
+      $("#classMembers").append(
+        "<span class='users list-group-item'>" +
+        user.username +
+        "</span>"
+      );
+    }
+    else {
+      $("#classMembers").prepend(
+        "<span class='users list-group-item'>" +
+        user.username +
+        "</span>"
+      );
+    }
+
+    if (isTA()) {
+      if (isFirstLoad) {
+        $("#userList").append(
+          "<span class='users list-group-item'>" +
+          "<span class='userName'>" + user.username + "</span>" +
+          "<button class='deleteUser pull-right btn btn-danger btn-xs'>Delete</button>" +
+          "</span>"
+        );
+      }
+      else {
+        $("#userList").prepend(
+          "<span class='users list-group-item'>" +
+          "<span class='userName'>" + user.username + "</span>" +
+          "<button class='deleteUser pull-right btn btn-danger btn-xs'>Delete</button>" +
+          "</span>"
+        );
+      }
+
+      $(".deleteUser").click(function () {
+        var userName = $(this).parent().find(".userName").text();
+        refClassUsers.orderByChild("username").equalTo(userName).once("value").then(function (user1) {
+          user1.forEach(function (user2) {
+            refClassUsers.child(user2.key).remove();
+          });
+        });
+
+      });
+    }
+    $("#memberCount").text(memberCount);
+  });
+
+  $("#searchUserBtn").click(function () {
+    isFirstLoad = false;
+    var userName = $("#searchUserInput").val();
+    ref = firebase.database().ref("users/");
+    ref.orderByChild("username").equalTo(userName).once("value").then(function (snapshot) {
+      snapshot.forEach(function (user) {
+        firebase.database().ref("classes/" + classID + "/users/" + user.key).set({
+          username: user.val().username,
+          user_id: user.key
+        });
+      });
+    });
+  });
+
+  refClassUsers.orderByChild("username").on("child_removed", function (data) {
+    $(".users:contains(" + data.val().username + ")").remove();
+    memberCount--;
+    $("#memberCount").text(memberCount);
+  });
+
+  $("#memberCount").text(memberCount);
+}
+
+// Change this later to check if person is actually a TA
+function isTA() {
+  return true;
 }
 
 function getClassID() {
@@ -35,8 +187,10 @@ function getClassID() {
   classID = pageURL[1]
 }
 
+var refClassUsers
 var classID;
 var memberCount = 0;
+var isFirstLoad = true;
 getClassID();
 
 initializeFirebase();
