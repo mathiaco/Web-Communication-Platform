cebde3fe650c23afde8dbd71fc217cd67d8c1938
Jeff Buonamici <jbuonamici@outlook.com>
2017-03-24 18:41:00 -0400

    Classes page will only show classes you are in. Now you can join classes after searching for them.
    #35

--- Project Folder/public/js/classes.js
+++ Project Folder/public/js/classes.js
@@ -34,8 +34,9 @@ $("#createClassBtn").click(function () {
 });
 
 
+// OLD
 // When class is added to database it'll display it
-classesRef.on("child_added", function (snapshot, prevChildKey) {
+/*classesRef.on("child_added", function (snapshot, prevChildKey) {
     var newClass = snapshot.val();
     var color;
 
@@ -53,11 +54,11 @@ classesRef.on("child_added", function (snapshot, prevChildKey) {
         colCount = 1;
     }
 
-    
+
 
     // append a new class with a specific key
     $("#classRow").append(
-        "<div id="+snapshot.getKey()+" class='col-lg-4'>" +
+        "<div id=" + snapshot.getKey() + " class='col-lg-4'>" +
         "<div class='panel panel-" + color + "'>" +
         "<div class='panel-heading'>" +
         newClass.title +
@@ -70,22 +71,35 @@ classesRef.on("child_added", function (snapshot, prevChildKey) {
         "<a href='/classpage?c=" + snapshot.getKey() + "' class='btn btn-default btn-default btn-block'>View</a>" +
         "</div>"
     )
-    
-});
+
+});*/
+
 // delete the specific class from the database and also the class will not be showed anymore on the webpage.
 function deleteBox(id) {
-  classesRef.child(id).remove();
-  var idOfBox = id;
-  $("div").remove("#" + idOfBox);
+    classesRef.child(id).remove();
+    var idOfBox = id;
+    $("div").remove("#" + idOfBox);
 };
 
+// allows user to join class when button is pressed
+function joinClass(classID) {
+    ref = firebase.database().ref("users/");
+    ref.orderByChild("user_id").equalTo(currentUserID).once("value").then(function (snapshot) {
+      snapshot.forEach(function (user) {
+        firebase.database().ref("classes/" + classID + "/users/" + user.key).set({
+          username: user.val().username,
+          user_id: user.key
+        });
+      });
+    });
+}
 
 // Ability to search for class
 $("#classSearchBtn").click(function () {
-    
+
     var className = $("#classSearchInput").val();
     ref = firebase.database().ref("classes/");
-    
+
     // Finds the class you search based on the text input
     ref.orderByChild("title").equalTo(className).once("value").then(function (snapshot) {
         snapshot.forEach(function (classSearched) {
@@ -99,7 +113,7 @@ $("#classSearchBtn").click(function () {
                 "<p>" + classSearched.val().description + "</p>" +
                 "</div>" +
                 "<div class='panel-footer'>" +
-                "<button type='button' class='btn btn-success btn-default btn-block'>Join</button>" +
+                "<button id='" + classSearched.getKey() + "' type='button' class='btn btn-success btn-default btn-block' onClick='joinClass(this.id)'>Join</button>" +
                 "</div>" +
                 "</div>"
             )
@@ -107,3 +121,41 @@ $("#classSearchBtn").click(function () {
     });
 });
 
+firebase.database().ref("classes/").orderByChild("users/" + currentUserID + "/user_id").equalTo(currentUserID).once("value").then(function (snapshot) {
+    var color;
+    snapshot.forEach(function (childSnapshot) {
+    var newClass = childSnapshot.val();
+
+    // Changes color of panel heading for fun
+    if (colCount == 1) {
+        color = "purple";
+        colCount++;
+    }
+    else if (colCount == 2) {
+        color = "pink";
+        colCount++;
+    }
+    else if (colCount == 3) {
+        color = "orange";
+        colCount = 1;
+    }
+
+
+
+    // append a new class with a specific key
+    $("#classRow").append(
+        "<div id=" + childSnapshot.getKey() + " class='col-lg-4'>" +
+        "<div class='panel panel-" + color + "'>" +
+        "<div class='panel-heading'>" +
+        newClass.title +
+        "<button id='" + childSnapshot.getKey() + "' class='btn btn-xs btn-danger pull-right' onClick='deleteBox(this.id)'><i class='fa fa-times'></i></button>" +
+        "</div>" +
+        "<div class='panel-body'>" +
+        "<p>" + newClass.description + "</p>" +
+        "</div>" +
+        "<div class='panel-footer'>" +
+        "<a href='/classpage?c=" + childSnapshot.getKey() + "' class='btn btn-default btn-default btn-block'>View</a>" +
+        "</div>"
+    )
+    })
+});
